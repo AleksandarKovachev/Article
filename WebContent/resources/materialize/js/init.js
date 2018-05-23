781b75e598c32896a9cec24c4bfff7a338e986e5
AleksandarKovachev <akovachev@tu-sofia.bg>
2018-05-23 22:49:30 +0300

    Upgrade the Materialize CSS version.

--- WebContent/resources/materialize/js/init.js
+++ WebContent/resources/materialize/js/init.js
@@ -1,84 +1,63 @@
-(function($) {
-	$(function() {
-		$('.button-collapse').sideNav();
-		$('.parallax').parallax();
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(
+		function() {
+			$('.sidenav').sidenav();
+			$('.parallax').parallax();
+			$('select').formSelect();
+			$('.modal').modal();
+			$(".dropdown-button").dropdown();
 
-$(document).ready(function() {
-	$('.dropdown-button2').dropdown({
-		inDuration : 300,
-		outDuration : 225,
-		constrain_width : false,
-		hover : true,
-		gutter : ($('.dropdown-content').width() * 3) / 2.5 + 5,
-		belowOrigin : true,
-		alignment : 'left'
-	});
-});
+			$('.dropdown-button2').dropdown({
+				inDuration : 300,
+				outDuration : 225,
+				constrain_width : false,
+				hover : true,
+				gutter : ($('.dropdown-content').width() * 3) / 2.5 + 5,
+				belowOrigin : true,
+				alignment : 'left'
+			});
 
-// select
-$(document).ready(function() {
-	$('select').material_select();
+			$("select[required]").css({
+				position : 'absolute',
+				display : 'inline',
+				height : 0,
+				padding : 0,
+				width : 0
+			});
 
-	$("select[required]").css({
-		position : 'absolute',
-		display : 'inline',
-		height : 0,
-		padding : 0,
-		width : 0
-	});
+			$('.tooltipped').tooltip({
+				delay : 50
+			});
 
-	$(".dropdown-button").dropdown();
-});
+			$('input#input_text, textarea#textarea').characterCounter();
 
-// init modal
-$(document).ready(function() {
-	// the "href" attribute of .modal-trigger must specify the modal ID that
-	// wants to be triggered
-	$('.modal').modal();
-});
+			$('.collapsible').collapsible();
 
-$(document).ready(function() {
-	$('.tooltipped').tooltip({
-		delay : 50
-	});
-});
+			$(".closebtn").click(function() {
+				this.parentElement.style.display = 'none';
+			});
 
-$(document).ready(function() {
-	$('input#input_text, textarea#textarea').characterCounter();
-});
-
-$(document).ready(function() {
-	$('.collapsible').collapsible();
-});
-
-// Datas
-$('.datepicker').pickadate(
-		{
-			monthsFull : [ 'Януари', 'Февруари', 'Март', 'Април', 'Май', 'Юни',
-					'Юли', 'Август', 'Септември', 'Октомври', 'Ноември',
-					'Декември' ],
-			monthsShort : [ 'Януари', 'Февруари', 'Март', 'Април', 'Май',
-					'Юни', 'Юли', 'Август', 'Септември', 'Октомври', 'Ноември',
-					'Декември' ],
-			weekdaysFull : [ 'Неделя', 'Понеделник', 'Вторник', 'Сряда',
-					'Четвъртък', 'Петък', 'Събота' ],
-			weekdaysShort : [ 'Неделя', 'Понеделник', 'Вторник', 'Сряда',
-					'Четвъртък', 'Петък', 'Събота' ],
-			today : 'Днес',
-			clear : 'Изчисти',
-			close : 'Затвори',
-			labelMonthNext : 'Следващ месец',
-			labelMonthPrev : 'Предишен месец',
-			labelMonthSelect : 'Избери месец',
-			labelYearSelect : 'Избери година',
-			selectMonths : true,
-			selectYears : 15,
-			format : 'dd.mm.yyyy',
-			formatsubmit : 'dd.mm.yyyy'
+			$('.datepicker').datepicker(
+					{
+						monthsFull : [ 'Януари', 'Февруари', 'Март', 'Април',
+								'Май', 'Юни', 'Юли', 'Август', 'Септември',
+								'Октомври', 'Ноември', 'Декември' ],
+						monthsShort : [ 'Януари', 'Февруари', 'Март', 'Април',
+								'Май', 'Юни', 'Юли', 'Август', 'Септември',
+								'Октомври', 'Ноември', 'Декември' ],
+						weekdaysFull : [ 'Неделя', 'Понеделник', 'Вторник',
+								'Сряда', 'Четвъртък', 'Петък', 'Събота' ],
+						weekdaysShort : [ 'Неделя', 'Понеделник', 'Вторник',
+								'Сряда', 'Четвъртък', 'Петък', 'Събота' ],
+						today : 'Днес',
+						clear : 'Изчисти',
+						close : 'Затвори',
+						labelMonthNext : 'Следващ месец',
+						labelMonthPrev : 'Предишен месец',
+						labelMonthSelect : 'Избери месец',
+						labelYearSelect : 'Избери година',
+						selectMonths : true,
+						selectYears : 15,
+						format : 'dd.mm.yyyy',
+						formatsubmit : 'dd.mm.yyyy'
+					});
 		});
-
-$(".closebtn").click(function() {
-	this.parentElement.style.display = 'none';
-});
